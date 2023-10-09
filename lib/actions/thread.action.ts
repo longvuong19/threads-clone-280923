4dad695dece85515fcd474fee340bc71569c3478
LongVuong <longvuong1920@gmail.com>
2023-10-09 11:24:44 +0700

    add Thread Card component

--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -38,3 +38,39 @@ export async function createThread({
 
   revalidatePath(path);
 }
+
+export async function fetchPost(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB();
+
+    // Calculate the number of posts to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Fetch posts that have no parents (top-level)
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+
+    const totalPostsCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
+  } catch (error: any) {
+    throw new Error(`Error: ${error.message}`);
+  }
+}
